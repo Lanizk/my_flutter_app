ee618c2acd7e3606939180c2bb66bd5abd4cd729
Lanizk <113615198+Lanizk@users.noreply.github.com>
2025-06-05 15:03:18 +0300

    completed the edit screen.Now working on storage of the screen

--- lib/features/communityr/screens/community_screen.dart
+++ lib/features/communityr/screens/community_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_flutter_app/common/error_text.dart';
 import 'package:my_flutter_app/common/loader.dart';
+import 'package:my_flutter_app/features/auth/controller/auth_conroller.dart';
 import 'package:my_flutter_app/features/communityr/controller/communitycontroller.dart';
+import 'package:routemaster/routemaster.dart';
 
 class communityScreen extends ConsumerWidget {
   final String name;
@@ -12,8 +14,14 @@ class communityScreen extends ConsumerWidget {
     required this.name,
   });
 
+
+  void navigateToModTools(BuildContext context)
+  {
+    Routemaster.of(context).push('/mod-tools/$name');
+  }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user=ref.watch(userProvider)!;
     return Scaffold(
       body: ref.watch(getCommunityByNameProvider(name)).when(
           data: (community) => NestedScrollView(
@@ -21,11 +29,64 @@ class communityScreen extends ConsumerWidget {
                 return [
                   SliverAppBar(
                     expandedHeight: 150,
+                    floating: true,
+                    snap: true,
                     flexibleSpace: Stack(
                       children: [
-                        Positioned.fill(child: Image.network(community.banner))
+                        Positioned.fill(
+                          child: Image.network(community.banner,
+                              fit: BoxFit.cover),
+                        )
                       ],
                     ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(16),
+                    sliver: SliverList(
+                        delegate: SliverChildListDelegate([
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(community.avatar),
+                          radius: 35,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'r/${community.name}',
+                            style: const TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          community.mods.contains(user.uid)?
+                          OutlinedButton(
+                              onPressed: () {navigateToModTools(context);},
+                              style:ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+
+                                ),
+                                padding: const EdgeInsets.symmetric(horizontal: 25),
+                              ),
+                              child: const Text('Mod Tools')):OutlinedButton(
+                              onPressed: () {},
+                              style:ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+
+                                ),
+                                padding: const EdgeInsets.symmetric(horizontal: 25),
+                              ),
+                              child: Text(community.members.contains(user.uid)? 'Joined':'Join')),
+                        ],
+                      ),
+                          Padding(padding: const EdgeInsets.only(top:10),
+                          child: Text('${community.members.length} members'),)
+                    ])),
                   )
                 ];
               },
--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,8 @@ import 'package:my_flutter_app/features/auth/screen/home/home_screen.dart';
 import 'package:my_flutter_app/features/auth/screen/login_screen.dart';
 import 'package:my_flutter_app/features/communityr/screens/community_screen.dart';
 import 'package:my_flutter_app/features/communityr/screens/create_community_screens.dart';
+import 'package:my_flutter_app/features/communityr/screens/edit_community_screen.dart';
+import 'package:my_flutter_app/features/communityr/screens/mod_tools_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final logedOutRoute =
@@ -15,4 +17,10 @@ final logedInRoute = RouteMap(routes: {
           child: communityScreen(
         name: route.pathParameters['name']!,
       )),
+  '/mod-tools/:name': (routeData) => MaterialPage(child: ModToolsScreen(
+    name:routeData.pathParameters['name']!,
+  )),
+  '/edit-community/:name': (routeData) => MaterialPage(child: EditCommunityScreen(
+    name:routeData.pathParameters['name']!,
+  )),
 });
